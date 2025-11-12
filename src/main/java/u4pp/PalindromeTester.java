9735feac96857303a87c8484a8e454c7bcf1c8e6
LifeofLarryD <paulyflash17@gmail.com>
2025-11-12 07:57:45 +0000

    I made a scanner where it checks if the word you put is a palindrome and then I created a game where yo can play rock paper scissors with the computer

--- src/main/java/u4pp/PalindromeTester.java
+++ src/main/java/u4pp/PalindromeTester.java
@@ -9,14 +9,64 @@ public class PalindromeTester {
      * YOUR JAVADOC HERE
      */
     public static void execute(Scanner sc) {
-        /* YOUR CODE HERE */
+        System.out.println("Welcome to Palindrome Tester");
+        
+        boolean keepGoing=true;
+        while (keepGoing==true){
+            System.out.print("Enter a phrase: ");
+            String phrase = sc.nextLine();
+        
+        if (isPalindrome(phrase)==true){
+            System.out.println(phrase + " is a palindrome");
+        }
+        else{
+            System.out.println(phrase + " is not a palindrome");
+        }
+        keepGoing = getYesOrNoInput(sc, "Keep testing? (Y)es or (N)o: ");
+    }
     }
 
     /**
      * YOUR JAVADOC HERE
      */
     public static boolean isPalindrome(String phrase) {
-        /* YOUR CODE HERE */
+        if(phrase==null){
+            return false;
+        }
+        String cleaned = cleanString(phrase);
+        int left = 0;
+        int right = cleaned.length()- 1;
+
+        while (left < right){
+            if (cleaned.charAt(left) != cleaned.charAt(right)){
+                return false;
+            }
+            left += 1;
+            right -= 1;
+        }
+        return true;
+    }
+
+    private static String cleanString(String phrase){
+        return phrase.replaceAll("[^A-Za-z]", "").toLowerCase();
+    }
+
+    private static boolean getYesOrNoInput(Scanner sc, String p) {
+        while(true){
+            System.out.print(p);
+            String input = sc.nextLine().trim();
+
+            if ((input.equalsIgnoreCase("y")) || input.equalsIgnoreCase("yes")){
+                return true;
+            }
+            else if ((input.equalsIgnoreCase("n")) || (input.equalsIgnoreCase("no"))){
+                return false;
+            }
+            else{
+                System.out.println("Invalid Input, please try again");
+            }
+
+        }
     }
 
     /* Any helper methods you might need */
--- src/main/java/u4pp/RockPaperScissors.java
+++ src/main/java/u4pp/RockPaperScissors.java
@@ -13,14 +13,45 @@ public class RockPaperScissors {
      * JAVADOC HERE
      */
     public static void play(Scanner sc) {
-        /* PART B: YOUR CODE HERE */
+        System.out.println("Welcome to Rock Paper Scissors");
+        int w = 0;
+        int l = 0;
+        int t = 0;
+
+        boolean keepPlaying = true;
+        while (keepPlaying==true){
+            int result = noog(sc);
+            
+            if (result == 1){
+                w += 1;
+            }
+            else if (result == 0){
+                t += 1;
+            }
+            else{
+                l += 1;
+            }
+            System.out.println("You have " + w + " wins and " + l + " losses and " + t + " ties");
+            keepPlaying = getYesOrNoInput(sc, "Would you like to play again? (Y)es or (N)o: ");
+        }
+        
     }
 
     /**
      * JAVADOC HERE
      */
     public static int results(String playerChoice, String computerChoice) {
-        /* PART A: YOUR CODE HERE */
+        playerChoice = playerChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+        if(playerChoice.equals(computerChoice)){
+            return 0;
+        }
+        if((playerChoice.equals("r")) && (computerChoice.equals("s")) || (playerChoice.equals("p")) && (computerChoice.equals("r")) || (playerChoice.equals("s")) && (computerChoice.equals("p"))){
+            return 1;
+        }
+        else{
+            return -1;
+        }
     }
 
     /* 
@@ -34,4 +65,81 @@ public class RockPaperScissors {
      * 4. playRound(Scanner sc, int[] scores) - handles one complete round of RPS
      * 5. formatChoice(String choice) - converts "r" to "Rock", "p" to "Paper", etc.
      */
+
+private static String k(){
+    int q = (int)(Math.random() * 3);
+    if (q == 0){
+        return "r";
+    }
+    else if (q == 1){
+        return "p";
+    }
+    else{
+        return "s";
+    }
+}
+
+private static String person(Scanner sc){
+    while (true){
+        System.out.print("Would you like to pick (R)ock, (P)aper, (S)cissors, or (A)ny: ");
+        String h = sc.nextLine().toLowerCase();
+        if((h.equals("r")) || (h.equals("p")) || (h.equals("s")) || (h.equals("a"))){
+            return h;
+        }
+        else{
+            System.out.println("Invalid input, please try again");
+        }
+    }
+}
+
+private static int noog(Scanner sc){
+    String playerChoice = person(sc);
+    if (playerChoice.equals("a")){
+        playerChoice = k();
+    }
+    String computerChoice = k();
+    int e = results(playerChoice, computerChoice);
+    if (e==0){
+        System.out.println("You both picked" + formatChoice(playerChoice) + ". It's a tie!");
+    }
+    else if ( e == 1){
+        System.out.println("You picked" + formatChoice(playerChoice) + ". and the Computer picked " + formatChoice(computerChoice) + ". You win!");
+    }
+    else{
+        System.out.println("You picked" + formatChoice(playerChoice) + ". and the Computer picked " + formatChoice(computerChoice) + ". You lose!");
+    }
+    return e;
+}
+
+private static boolean getYesOrNoInput(Scanner sc, String Prompt){
+    while (true){
+        System.out.print(Prompt);
+        String input = sc.nextLine().trim().toLowerCase();
+        if(input.equals("y")){
+            return true;
+        }
+        else if (input.equals("n")){
+            return false;
+        }
+        else{
+            System.out.println("Invalid Input, please try again");
+        }
+        }
+    }
+
+private static String formatChoice(String choice){
+    choice = choice.toLowerCase();
+    if(choice.equals("r")){
+        return "Rock";
+    }
+    if (choice.equals("p")){
+        return "Paper";
+    }
+    if (choice.equals("s")){
+        return "Scissors";
+    }
+    else{
+        return "Invalid";
+    }
+}
 }
